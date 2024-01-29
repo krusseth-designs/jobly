4fdca7cbdde7dc14b7154009c31df3fb01fd6981
Karen Russeth <66326669+krusseth-designs@users.noreply.github.com>
2024-01-29 10:23:17 -0500

    Add files via upload

---
+++ build/precache-manifest.bb56f88f5d6fc84aa12b6fb941c6932e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f853fe2125fd3ca0e72a",
+    "url": "/static/css/main.5e026400.chunk.css"
+  },
+  {
+    "revision": "f853fe2125fd3ca0e72a",
+    "url": "/static/js/main.f853fe21.chunk.js"
+  },
+  {
+    "revision": "f6b5790b54d3f29f070b",
+    "url": "/static/css/1.b6b49c6a.chunk.css"
+  },
+  {
+    "revision": "f6b5790b54d3f29f070b",
+    "url": "/static/js/1.f6b5790b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f47f1aec55296ea44f76832ad3c6c03",
+    "url": "/index.html"
+  }
+];
